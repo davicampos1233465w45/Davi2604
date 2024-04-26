8b169865c00f7b537eb6accc5c6f96728c7e21ae
davicampos1233465w45 <162125285+davicampos1233465w45@users.noreply.github.com>
2024-04-26 11:44:28 -0300

    Add files via upload

---
+++ Davi2604.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Davi2604 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sal_carlos, sal_joao;
+		int meses_NUMERICO;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+
+	}
+
+}
